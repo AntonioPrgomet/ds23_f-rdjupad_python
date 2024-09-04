0c08f9c04bfd914e432c25bead100575731d6dc6
Linus Rundberg Streuli <linus.rundberg.streuli@gmail.com>
2024-09-04 13:14:07 +0200

    lägg till presentationer om moduler och loggning

--- lektion_3/test.py
+++ lektion_3/test.py
@@ -1,8 +1,14 @@
+import pytest
+
 def add_one(x):
     return x + 1
 
 def test_add_one():
     assert add_one(3) == 4
+
+def test_add_one_with_str():
+    with pytest.raises(TypeError):
+        add_one('4')
 
 def test_add_one_fail():
     assert add_one(4) == 7
