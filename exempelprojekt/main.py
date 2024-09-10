651eb49c6848aa06658ba7699d75c74aade47d40
Linus Rundberg Streuli <linus.rundberg.streuli@gmail.com>
2024-09-10 13:05:06 +0200

    ändra namn på logg-filen

--- exempelprojekt/main.py
+++ exempelprojekt/main.py
@@ -6,7 +6,7 @@ from datasaver import DataSaver
 logger = logging.getLogger(__name__)
 
 logging.basicConfig(
-    filename='c:/Users/linus/dev/test_log.log', 
+    filename='pipeline.log', 
     format='[%(asctime)s][%(name)s] %(message)s', 
     datefmt='%Y-%m-%d %H:%M:%S', 
     level=logging.INFO)
